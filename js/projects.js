005353b29a33edc333a1938b0565a967ae0ee6b5
mhmd-k <mhmdk425@gmail.com>
2023-03-23 17:25:21 +0300

    add a react project and delete some files

--- js/projects.js
+++ js/projects.js
@@ -1,11 +1,11 @@
-export const projects = [
+[
   {
     image: "/images/vanLife.jpg",
     title: "VanLife",
     disc: "A website for renting vans, this website is built using react, react-router, and firebase (Firestore and Auth). Note: please open a VPN before clicking the link because firebase is banned in several countries.",
     tech: "React firebase(Firestore and Auth)",
     github: "https://github.com/mhmd-k/VanLife",
-    link: "https://vanlife11.netlify.app",
+    link: "https://vanlife11.netlifys.app",
     dataFilter: "react",
   },
   {
